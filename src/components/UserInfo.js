e4b7bb9f50bc61ab522fba29e4761e79278e120c
Alyssa Mae Bautista <alyssamae@Alyssas-MBP.lan>
2024-07-23 18:25:50 -0700

    9

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,24 +1,24 @@
-// export default class UserInfo {
-//     constructor(nameEl, descriptionEl, avatarEl) {
-//         this._nameEl = document.querySelector(".profile__title");
-//         this._descriptionEl = document.querySelector(descriptionEl);
-//         this._link = document.querySelector(avatarEl);
-//     }
+export default class UserInfo {
+    constructor(nameEl, descriptionEl, avatarEl) {
+        this._nameEl = document.querySelector(nameEl);
+        this._descriptionEl = document.querySelector(descriptionEl);
+        this._link = document.querySelector(avatarEl);
+    }
 
-//     getUserInfo() {
-//         return {
-//             name: this._nameEl.textContent,
-//             description: this._descriptionEl.textContent,
-//             avatar: this._link.src,
-//         }
-//     }
+    getUserInfo() {
+        return {
+            name: this._nameEl.textContent,
+            description: this._descriptionEl.textContent,
+            avatar: this._link.src,
+        }
+    }
 
-//     setUserInfo(name, description) {
-//         this._nameEl.textContent = name;
-//         this._descriptionEl.textContent = description;
-//     }
+    setUserInfo(name, description) {
+        this._nameEl.textContent = name;
+        this._descriptionEl.textContent = description;
+    }
 
-//     setAvatarImage(link) {
-//         this._link.src = link;
-//     }
-// }
+    setAvatarImage(link) {
+        this._link.src = link;
+    }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -60,11 +60,11 @@ const setIsLiked = (card) => {
 }
 
 //UserInfo
-const userInfo = new UserInfo({
-    nameEl: selectors.profileTitle, 
-    descriptionEl: selectors.profileDescription, 
-    avatarEl: selectors.profileAvatar,
-});
+const userInfo = new UserInfo(
+    selectors.profileTitle, 
+    selectors.profileDescription, 
+    selectors.profileAvatar,
+);
 
 
 Promise.all([api.fetchUserInfo(), api.getInitialCards()])
